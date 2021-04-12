78d128c194aa67281ae2ae4a4794562d4acce228
bemuse01 <sequeland@gmail.com>
2021-04-12 12:21:30 +0900

    black to white

--- assets/js/application/shape/plane/shape.plane.param.js
+++ assets/js/application/shape/plane/shape.plane.param.js
@@ -1,6 +1,6 @@
 SHAPE.plane.param = class{
     constructor(){
-        this.color = 0xffffff
+        this.color = 0x000000
         this.seg = 1
         this.damping = 0.999
         this.minDist = 0.0002
