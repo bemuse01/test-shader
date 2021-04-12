8b61a5586513ddb28ee5fd575c63ed1470e32cde
bemuse01 <sequeland@gmail.com>
2021-04-12 18:06:46 +0900

    Update shape.plane.param.js

--- assets/js/application/shape/plane/shape.plane.param.js
+++ assets/js/application/shape/plane/shape.plane.param.js
@@ -2,7 +2,7 @@ SHAPE.plane.param = class{
     constructor(){
         this.color = 0x000000
         this.seg = 1
-        this.damping = 0.999
+        this.damping = 0.9975
         this.minDist = 0.0002
     }
 }
